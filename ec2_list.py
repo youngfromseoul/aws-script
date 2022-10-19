8cf5d1e453d4a720d1d0075c2d1a451713b3eb80
Hyoyoung kim <45892745+youngfromseoul@users.noreply.github.com>
2022-10-19 13:27:24 +0900

    Add files via upload

---
+++ ec2_list.py
@@ -0,0 +1,56 @@
+import boto3
+from openpyxl import Workbook
+from datetime import datetime
+
+data_list = []
+
+# profile name 지정
+session = boto3.Session(profile_name='dev')
+client = session.client('ec2')
+paginator = client.get_paginator('describe_instances')
+response_iterator = paginator.paginate()
+
+# page별 반복
+for page in response_iterator:
+    # page 내의 Reservations 내의 Instances 반복하며 인스턴스 정보 출력
+    for j in page['Reservations']:
+        print(j)
+        for i in j['Instances']:
+            
+            NameTag = ''
+            # 인스턴스 정보 안에 Tags 정보가 있는 경우만
+            if 'Tags' in i:
+                # Tags 정보 중 Name Key를 찾아 Value값 반환해서 저장
+                for tag in i['Tags']:
+                    if tag['Key'] == 'Name':
+                        NameTag = tag['Value']
+            
+            data_tuple = (
+                # instance id
+                i['InstanceId'],
+                # instance platform (ex, linux/unix)
+                i['PlatformDetails'],
+                # instance type (ex, t3.medium)
+                i['InstanceType'],
+                # 생성 시간 > time형식 변경 필요
+                i['LaunchTime'].strftime('%Y-%m-%d'),
+                # Name Tag 값
+                NameTag,
+                # 현재 State (ex, running)
+                i['State']['Name']
+            )
+            data_list.append(data_tuple)
+
+
+# excel file로 저장
+wb = Workbook()
+ws = wb.active
+
+header = ('InstanceId', 'Platform', 'InstanceType', 'LaunchTime', 'NameTag', 'State')
+ws.append(header)
+for data in data_list:
+    ws.append(data)
+
+# 현재 시간을 사용해 file 저장
+excel_name = 'ec2_list_' + datetime.now().strftime('%Y%m%d%H%M') + '.xlsx'
+wb.save(excel_name)
---
+++ lc_list.py
@@ -0,0 +1,117 @@
+import boto3
+import logging
+import sys
+import argparse
+from botocore.exceptions import ClientError
+from openpyxl import Workbook
+from datetime import datetime
+
+# Arguments
+parser = argparse.ArgumentParser(description='Launch Configurations')
+parser.add_argument("-p",  "--profile", help='dev or prod')
+args = parser.parse_args()
+
+if args.profile is None:
+    parser.error("-p or --profile requires")
+
+# profile
+aws_profile = args.profile
+aws_region = 'ap-northeast-2'
+session = boto3.Session(profile_name=aws_profile)
+
+print ("env:", session.profile_name)
+
+# Logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# describe_launch_configurations
+def describe_launch_configurations(region, lc_name):
+    client = session.client('autoscaling')
+    paginator = client.get_paginator('describe_launch_configurations')
+    response_iterator = paginator.paginate(
+       LaunchConfigurationNames=[lc_name]
+    )
+    try:
+        for page in response_iterator:
+            for j in page['LaunchConfigurations']:
+                sg = ', '.join(j['SecurityGroups'])
+                data_tuple = (
+                    j['LaunchConfigurationName'],
+                    j['ImageId'],
+                    j['KeyName'],
+                    sg,
+                    j['InstanceType'],
+                    j['IamInstanceProfile']
+                )
+        return data_tuple
+
+    except ClientError as e:
+        message = 'Error getting list of launch configurations: {}'.format(e)
+        logger.error(message)
+
+    return {}
+
+# describe_auto_scaling_groups
+def describe_auto_scaling_groups(region):
+    logger.info('Getting Launch Configurations In-Use for ASG: {}'.format(region))
+    client = session.client('autoscaling')
+    paginator = client.get_paginator('describe_auto_scaling_groups')
+    response_iterator = paginator.paginate()
+    data_list = []
+    try: 
+        for page in response_iterator:
+            for j in page['AutoScalingGroups']:
+                if 'LaunchConfigurationName' in j:
+                    asg_name = j['AutoScalingGroupName']
+                    lc_name = j['LaunchConfigurationName']
+                    lc_tuple = describe_launch_configurations(aws_region, lc_name) 
+                    data_tuple = (
+                        asg_name,
+                        lc_name
+                    )
+                    murge_tuple = data_tuple + lc_tuple 
+                    data_list.append(murge_tuple)
+        return data_list
+        
+    except ClientError as e:
+        message = 'Error getting list of autoscaling group: {}'.format(e)
+        logger.error(message)
+
+    return {}
+
+def write_excel(file):
+    # excel 저장
+    wb = Workbook()
+    ws = wb.active
+
+    header = ('AutoScalingGroupName', 'LaunchConfigurationName', 'LaunchConfigurationName', 'ImageId', 'KeyName', 'SG', 'InstanceType', 'IamInstanceProfile')
+    ws.append(header)
+    for data in file:
+        ws.append(data)
+
+    # file 저장
+    excel_name = 'lc_list_' + datetime.now().strftime('%Y%m%d%H%M') + '.xlsx'
+    wb.save(excel_name)
+
+def main():
+
+    inventory = []
+
+    try:
+        inventory = describe_auto_scaling_groups(aws_region)
+        print(type(inventory[3]))
+    except ClientError as e:
+        message = 'Error getting inventory, check your credential configuration or try with the -r argument: {}'.format(e)
+        logger.error(message)
+
+    write_excel(inventory)
+    return inventory
+
+if __name__ == "__main__":
+    main()
